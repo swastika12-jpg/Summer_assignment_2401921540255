fd0874c1091e3c6cbb34c4932ecc43794633e209
Swastika dubey <swastikadubey4@gmail.com>
2026-06-02 18:58:23 +0530

    Create twosum.cpp

---
+++ week_1/day_1/twosum.cpp
@@ -0,0 +1,24 @@
+#day 1 solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+  
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+         }
+    }
+    } 
+    return {}; // koi pair nahi mila
+}
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9; 
+    vector<int> result = twoSum(nums, target);
+    cout << "Index 1: " << result[0] << endl;
+    cout << "Index 2: " << result[1] << endl;
+    return 0;
+}
