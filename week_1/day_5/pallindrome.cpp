764dd541f9a1dc222adbd547bae06d468d38cfe9
Swastika dubey <swastikadubey4@gmail.com>
2026-06-07 22:03:04 +0530

    Create pallindrome.cpp

---
+++ week_1/day_5/pallindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        
+        while (left < right) {
+            // Skip non-alphanumeric from left
+            while (left < right && !isalnum(s[left]))
+                left++;
+            // Skip non-alphanumeric from right
+            while (left < right && !isalnum(s[right]))
+                right--;
+            
+            if (tolower(s[left]) != tolower(s[right]))
+                return false;
+            
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
